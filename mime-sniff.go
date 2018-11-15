1464aca6f850d03688fa9a9a4b4dd015d52617d5
Sachin Saini <iamsainisachin@gmail.com>
2018-11-15 14:17:27 +0530

    general refactor

--- mime-sniff.go
+++ mime-sniff.go
@@ -19,18 +19,12 @@ import (
 	"os"
 )
 
-func GetFileContentType(out *os.File) (string, error) {
-
-	// Only the first 512 bytes are used to sniff the content type.
+func getFileContentType(f *os.File) (string, error) {
 	buffer := make([]byte, 512)
-
-	_, err := out.Read(buffer)
+	_, err := f.Read(buffer)
 	if err != nil {
 		return "", err
 	}
-
-	// Use the net/http package's handy DectectContentType function. Always returns a valid
-	// content-type by returning "application/octet-stream" if no others seemed to match.
 	contentType := http.DetectContentType(buffer)
 
 	return contentType, nil
