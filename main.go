977b9e1188dbccd9f6de0ad70077257469b2038e
Sachin Saini <iamsainisachin@gmail.com>
2018-11-17 04:40:19 +0530

    cleanup

--- main.go
+++ main.go
@@ -15,111 +15,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
-	"path/filepath"
 )
 
 func main() {
-	dir := getPwd()
-	imgList := genFileSlice(dir)
-	willProceed := verifyImg(imgList)
-	if willProceed {
-		os.Mkdir(path.Join(dir, "dist"), 0700)
-		for _, f := range imgList {
-
-			fileExt := filepath.Ext(f)
-			switch fileExt {
-			case ".jpg", ".jpeg":
-				minifyJpeg(50, f, dir)
-
-			case ".png":
-				minifyPng(50, f, dir)
-			}
-
-		}
-	}
-
-	parseJSON()
-}
-
-func getPwd() string {
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	return pwd
-}
-
-func genFileSlice(d string) []string {
-	files, err := ioutil.ReadDir(d)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	var imgSlice []string
-
-	for _, f := range files {
-
-		fileExt := filepath.Ext(f.Name())
-		switch fileExt {
-		case ".jpg", ".jpeg", ".png":
-			imgSlice = append(imgSlice, f.Name())
-		}
-
-	}
-	return imgSlice
-}
-
-func verifyImg(fs []string) bool {
-	for _, file := range fs {
-		imgfile, err := os.Open(file)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		defer imgfile.Close()
-		contentType, err := getFileContentType(imgfile)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		switch contentType {
-		case "image/jpeg", "image/png":
-			fmt.Println("verified mime type of", file, "as", contentType)
-		default:
-			fmt.Println("invalid format ", contentType, " of file ", file)
-			os.Exit(1)
-		}
-	}
-	return true
-}
-
-// Config struct
-type Config struct {
-	Type        string
-	Compression int
-}
-
-func parseJSON() {
-	f, err := os.Open("config.json")
-
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	bv, _ := ioutil.ReadAll(f)
-
-	var conf []Config
-
-	json.Unmarshal([]byte(bv), &conf)
-
-	fmt.Println(conf)
-
+	// dir := getPwd()
+	// imgList := getFileList(dir)
+	// willProceed := verifyImg(imgList)
+	// proceedToCompression(willProceed, imgList)
+	// createConfigFile()
+	configFilePath, err := os.Getwd()
+	check(err)
+	initConfig(configFilePath)
 }
